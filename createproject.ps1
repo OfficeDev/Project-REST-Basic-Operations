6bbf8231c9d3e95cb42b10f3ffecd67ab58d7fb0
Nadin Merali <namerali@microsoft.com>
2016-08-10 13:55:49 -0700

    Update createproject.ps1

--- createproject.ps1
+++ createproject.ps1
@@ -1,3 +1,9 @@
+<#
+  Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+  See LICENSE in the project root for license information.
+#>
+
+
 # Creates a Project using ReST API
 param
 (
